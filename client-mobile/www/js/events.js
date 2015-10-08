0ea1b7c905b91a14a56473b879d997d258123d8c
Ryan Wholey <rjwholey@gmail.com>
2015-10-07 17:28:12 -0700

    chat factory initialized

--- client-mobile/www/js/events.js
+++ client-mobile/www/js/events.js
@@ -1,13 +1,49 @@
 angular.module('Pronto.events',[])
-	.controller('EventsCtrl',  ['$scope', 'SocketFactory', function ($scope, SocketFactory) {
+	.factory('EventsFactory', ['$rootScope', function ($rootScope) {
+
+		var eventList = [];
+
+		addEvent = function(eventName) {
+			eventList.push(eventName);
+		};
+
+		removeEvent = function(eventName) {
+			var removed = false;
+			for(var i = 0; i < eventList.length; i++){
+				if(eventList[i]===eventName){
+					eventList.splice(i,1);
+					removed = true;
+					break;
+				}
+			}
+			return removed;
+		};
+
+		getEvents = function(){
+			return eventList;
+		}
+
+		return {
+			addEvent : addEvent,
+			removeEvent : removeEvent,
+			getEvents : getEvents
+		}
+
+	}])
+	.controller('EventsCtrl', ['$scope', 'SocketFactory', 'EventsFactory', function ($scope, SocketFactory, EventsFactory) {
 		var eventFact = {};
-		$scope.eventObj = {'blurb':''};
+		$scope.eventObj = {blurb:''};
 
 		$scope.createEvent = function(){
 			console.log('create event');
 			eventFact.socket = SocketFactory.connect('event', $scope.eventObj.blurb);
-			console.log($scope.eventObj.blurb);
+			EventsFactory.addEvent($scope.eventObj.blurb);
 			$scope.eventObj.blurb = '';
 		};
-		
+
+		$scope.getEvents = function(){
+			$scope.events = EventsFactory.getEvents();
+		};
+
+
 	}])

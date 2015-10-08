2119fa51d3f9c8a8137542d5a0396478ebd9ad19
Ryan Wholey <rjwholey@gmail.com>
2015-10-07 17:00:21 -0700

    events template/ctrl

--- client-mobile/www/js/events.js
+++ client-mobile/www/js/events.js
@@ -1,5 +1,5 @@
 angular.module('Pronto.events',[])
-	.controller('EventsCtrl', ['$scope', 'SocketFactory', function ($scope, SocketFactory) {
+	.controller('EventsCtrl',  ['$scope', 'SocketFactory', function ($scope, SocketFactory) {
 		var eventFact = {};
 		$scope.eventVar = '';
 		eventFact.socket = SocketFactory.connect('event', $scope.blurb);
